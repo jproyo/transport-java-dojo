753c87c5d3f0d65d399db5f84f7620db5dd6b6d4
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-07-25 11:04:14 +0200

    Starting tdd

---
+++ src/test/java/transjprs/TransIntegrationTest.java
@@ -0,0 +1,14 @@
+package transjprs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransIntegrationTest {
+
+	@Test
+	public void testInputOutputSuccessfully() {
+		fail("Not yet implemented");
+	}
+
+}
