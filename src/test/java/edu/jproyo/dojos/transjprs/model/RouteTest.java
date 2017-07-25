7982f8b699b03d3dc90f0b446735ead8075b80ee
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-07-25 23:06:24 +0200

    tdd advance

--- src/test/java/edu/jproyo/dojos/transjprs/model/RouteTest.java
+++ src/test/java/edu/jproyo/dojos/transjprs/model/RouteTest.java
@@ -12,5 +12,21 @@ public class RouteTest {
 		assertEquals("B", Route.as("AB5").get().getTo());
 		assertEquals(new Integer(5), Route.as("AB5").get().getWeight());
 	}
+	
+	@Test
+	public void testAsStringSuccessWeightMoreThanOneDigit() {
+		assertEquals("A", Route.as("AB56").get().getFrom());
+		assertEquals("B", Route.as("AB57").get().getTo());
+		assertEquals(new Integer(53), Route.as("AB53").get().getWeight());
+	}
+
+	@Test
+	public void testAsStringFailOptionalEmpty() {
+		assertFalse(Route.as("AB").isPresent());
+		assertFalse(Route.as("A").isPresent());
+		assertFalse(Route.as("52AB").isPresent());
+		assertFalse(Route.as("ABAA5").isPresent());
+		assertFalse(Route.as("ABb1").isPresent());
+	}
 
 }
