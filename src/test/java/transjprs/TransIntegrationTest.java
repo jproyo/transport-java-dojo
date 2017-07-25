a284e2fdc3d06a83608306a238c96dd19f809729
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-07-25 11:17:25 +0200

    tdd advance

--- src/test/java/transjprs/TransIntegrationTest.java
+++ src/test/java/transjprs/TransIntegrationTest.java
@@ -8,7 +8,19 @@ public class TransIntegrationTest {
 
 	@Test
 	public void testInputOutputSuccessfully() {
-		fail("Not yet implemented");
+		StateResult result = TransService.create()
+			.calculateDistance(Route.as("A","B","C"))
+			.calculateDistance(Route.as("A","D"))
+			.calculateDistance(Route.as("A","D","C"))
+			.calculateDistance(Route.as("A","E","B","C","D"))
+			.calculateDistance(Route.as("A","E","D"))
+			.numberOfTrips(StartPoint.as("C"), FinishPoint.as("C"), Stops.atLeast(3))
+			.numberOfTrips(StartPoint.as("A"), FinishPoint.as("C"), Stops.exactly(4))
+			.shortestRouteLength("A", "C")
+			.shortestRouteLength("B", "B")
+			.numberOfRoutes(StartPoint.as("C"), FinishPoint.as("C"), Distance.less(30))
+			.collect();
+		assertNotNull(result);
 	}
 
 }
